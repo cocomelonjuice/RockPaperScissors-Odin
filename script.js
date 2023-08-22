95db29b86e1459c712edeae4de61ac8fc7d70cbe
cocomelonjuice <cocomelonjuice293@gmail.com>
2023-08-22 22:28:31 +0700

    code

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function game(playerSCore, computerSCore, moves) {
   
 function playGame ()
  
+
+
+// how to update repo github from terminal
+/*Quick summary:
+https://medium.com/@avivamazurek/how-to-update-a-github-repository-from-your-local-drive-e765eb48a691
+Open terminal and enter the folder of the github repository/project you want to update
+Type into terminal: “git add .” and then hit enter
+Type into terminal “git status” and then hit enter (this step is optional)
+Type into terminal “git commit -m ‘type any message here” and then hit enter
+Type into terminal “git push” and then hit enter */
