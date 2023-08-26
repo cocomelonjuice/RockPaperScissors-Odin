b3ac5af49c030496200269328139bea3523a0a14
cocomelonjuice <cocomelonjuice293@gmail.com>
2023-08-26 12:44:55 +0700

    update 26/08/2023

--- script.js
+++ script.js
@@ -1,26 +1,126 @@
 
-function game(/*playerscore, computerscore, moves*/) {
+const game = ()=> {
     let playerscore = 0;
     let computerscore = 0;
     let moves = 0;
 
-    function playGame (){
+const playGame = () =>{
         const rockBtn = document.querySelector ('.rock');
         const paperBtn = document.querySelector ('.paper');
         const scissorsBtn = document.querySelector ('.scissors');
-        const playerOptions = [rockBtn,paperBtn,scissorsBtn];
-        const computerOptions = ['rock','papers','scissors'];
+        const playeroptions = [rockBtn,paperBtn,scissorsBtn];
+        const computeroptions = ['rock','papers','scissors'];
 
-        playerOptions.forEach (option => {
-            option
+        playeroptions.forEach (option => {
+            option.addEventListener ('click', function (){
+                const movesleft = document.querySelector ('.movesleft');
+                moves++;
+                movesleft.innerText = `Move left: ${10 - moves}`;
+
+                const choicenumber = Math.floor(Math.random()*3);
+                const computerchoice = computeroptions[choicenumber];
+
+                winner(this.innerText, computerchoice);
+
+                if (moves == 10){
+                    gameOver(playeroptions, movesleft);
+                }
+            })
         })
-
 
     }    
 
+    const winner = (player,computer) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase ();
 
+        if (player === computer){
+            result.textContent = 'Draw';
+        }
 
+        else if (player == 'rock'){
+            if (computer == 'scissors'){
+                result.textContent = 'You won';
+                playerscore++;
+                playerScoreBoard.textContent = playerscore;
+            }
+            else{
+                result.textContent = 'You lost';
+                computerscore++
+                computerScoreBoard.textContent = computerscore;
+            }
+        }
+
+        else if (player == 'paper'){
+            if (computer == 'rock'){
+                result.textContent = 'You won';
+                playerscore++;
+                playerScoreBoard.textContent = playerscore;
+            }
+            else{
+                result.textContent = 'You lost';
+                computerscore++
+                computerScoreBoard.textContent = computerscore;
+            }
+        }
+
+        else if (player == 'scissors'){
+            if (computer == 'paper'){
+                result.textContent = 'You won';
+                playerscore++;
+                playerScoreBoard.textContent = playerscore;
+            }
+            else{
+                result.textContent = 'You lost';
+                computerscore++
+                computerScoreBoard.textContent = computerscore;
+            }
+        }
+    }
+
+    const gameOver = (playeroptions, movesleft) => {
+
+        const choosemove = document.querySelector ('.move');
+        const result = document.querySelector ('.result');
+        const reloadBtn = documetn.querySelector ('.reload');
+
+        playeroptions.forEach (option => {
+            option.style.display = 'none';
+        })
+
+        choosemove.innerText = 'Game over!';
+        movesleft.style.display = 'none';
+
+        if (playerscore >  computerscore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'You won the game';
+            result.style.color='green';
+        }
+        else if (playerscore <  computerscore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'You lost the game';
+            result.style.color='red';
+        }
+        else {
+            result.style.fontSize = '2rem';s
+            result.innerText = 'Draw';
+            result.style.color='gray';
+        }
+
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display='flex';
+        reloadBtn.addEventListener('click', ()=>{
+            window.location.reload();
+        })
+    }
+
+    playGame ();
 }
+
+game();
   
 
  
